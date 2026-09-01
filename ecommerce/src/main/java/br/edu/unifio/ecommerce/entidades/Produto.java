cba2892fa523c7066a5973b09af6239b4b46bb95
MartinssExe <gaclaudinomsilva@gmail.com>
2026-09-01 01:34:25 +0000

    confirmar

---
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Produto.java
@@ -0,0 +1,37 @@
+package br.edu.unifio.ecommerce.entidades;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Produto {
+@Id
+@GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    private String descricao;
+
+    private Short estoque;
+
+    private BigDecimal preco;
+
+    @ManyToOne
+    private Categoria categoria;
+
+
+
+
+
+
+}
